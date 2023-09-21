dbf1b66bee3eb9a0e06613c9b34a81f48737617e
winglechen <winglechen@gmail.com>
2023-09-21 18:30:04 +0800

    feature: add code style config file

---
+++ tests/resources/Simple.java
@@ -0,0 +1,25 @@
+package code;
+
+public class Simple {
+    private String var1;
+    private String var2;
+    private String var3;
+    private String var4;
+
+    public Simple(String var1, String var2) {
+
+    }
+
+    public void pubMethod1(String var1, String var2) {
+
+    }
+
+    public void pubMethod2(String var1, String var2) {
+
+    }
+
+    public void pubMethod3(String var1, String var2) {
+
+    }
+}
+
