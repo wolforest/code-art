08526cebf24efe0bfc14af217f2dd356d87a1733
winglechen <winglechen@gmail.com>
2023-09-25 15:13:04 +0800

    feature: add java pattern file

--- codeart/model/file.py
+++ codeart/model/file.py
@@ -1,29 +1,43 @@
 from collections import deque
 
-class ClassCounter(object):
-    className: str
-    staticAttributeNum: int = 0
-    attributeNume: int = 0
-
-class MethodCounter(object):
-    parameterNum: int = 0
+class CodeBlock(object):
     totalLines: int = 0
-    noIntentLines: int = 0
+
+    children = [] 
+
+class MethodBlock(CodeBlock):
+    parameterNum: int = 0
+
     maxIntentLevel: int = 0
+
+    ifBlockNum: int = 0
     '''
-    for/while/
+    for/while/foreach
     '''
     loopBlockNum: int = 0
 
-class FileCounter(object):
-    totalLines: int = 0
+class ClassBlock(CodeBlock):
+    className: str
 
+    staticAttributeNum: int = 0
+    attributeNum: int = 0
+
+    publicMethodNum: int = 0
+    privateMethodNum: int = 0
+    protectedMethodNum: int = 0
+
+    attributeList: list = []
+    methodList: list = []
+
+class FileBlock(CodeBlock):
     importNum: int = 0
     classNum: int = 0
     methodNum: int = 0
     maxClassLines: int = 0
     maxMethodLines: int = 0
 
+    classList: list = []
+    methodList: list = []
 
 
 class FileContext(object):
